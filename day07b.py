75e631ec9035ef6412d7933c06f436eeb1bb13cc
romeo <romeowp@gmail.com>
2019-12-11 00:46:02 +0100

    intcode computer refactor in progress

--- day07b.py
+++ day07b.py
@@ -1,21 +1,7 @@
-class Amplifier:
-    def __init__(self, intcode, phase):
+class IntcodeProgram:
+    def __init__(self, intcode, input_list):
         self.intcode = intcode
-        self.phase = phase
-        self.phase_is_set = False
-
-    def amplify(self, input_signal):
-        inputs = []
-        if not self.phase_is_set:
-            inputs.append(self.phase)
-            self.phase_is_set = True
-        inputs.append(input_signal)
-        return run_intcode(self.intcode, inputs)
-
-
-class IntcodeComputer:
-    def __init__(self, intcode):
-        self.intcode = intcode
+        self.input_list = input_list
         self.instruction_pointer = 0
 
     @property
@@ -26,23 +12,8 @@ class IntcodeComputer:
     def opcode(self):
         return self.instruction % 100
 
-    @property
-    def operation(self):
-        operation_per_code = {
-            1: self.add,
-            2: self.multiply,
-            3: self.save,
-            4: self.output,
-            5: self.jump_if_true,
-            6: self.jump_if_false,
-            7: self.less_than,
-            8: self.equals,
-            99: self.halt
-        }
-        return operation_per_code.get(self.opcode, self.halt)
-
     def parameter_mode(self, parameter_number):
-        return (self.instruction // (10 * (10 ** parameter_number * 10))) % 10
+        return (self.instruction // (10 ** (parameter_number + 1))) % 10
 
     def immediate_parameter(self, parameter_number):
         parameter_address = self.instruction_pointer + parameter_number
@@ -57,22 +28,33 @@ class IntcodeComputer:
         if mode == 0:
             return self.position_parameter(number)
         elif mode == 1:
-            return self.immediate_parameter(number)
+            return self.immediate_parameter(number)#
+
+    def write_to_parameter(self, parameter_number, value):
+        self.intcode[self.immediate_parameter(parameter_number)] = value
 
     def add(self):
-        self.intcode[self.immediate_parameter(3)] = self.parameter(1) + self.parameter(2)
+        value = self.parameter(1) + self.parameter(2)
+        self.write_to_parameter(3, value)
         self.instruction_pointer += 4
 
     def multiply(self):
-        self.intcode[self.immediate_parameter(3)] = self.parameter(1) * self.parameter(2)
+        value = self.parameter(1) * self.parameter(2)
+        self.write_to_parameter(3, value)
         self.instruction_pointer += 4
 
-    def save(self):
-        self.intcode[self.immediate_parameter(1)] = int(input())
+    def save_input(self):
+        if self.input_list:
+            value = self.input_list.pop(0)
+        else:
+            value = int(input())
+        self.write_to_parameter(1, value)
         self.instruction_pointer += 2
 
     def output(self):
-        return self.parameter(1)
+        value = self.parameter(1)
+        self.instruction_pointer += 2
+        return value
 
     def jump_if_true(self):
         if self.parameter(1):
@@ -83,66 +65,46 @@ class IntcodeComputer:
             self.instruction_pointer = self.parameter(2)
 
     def less_than(self):
-        self.intcode[self.immediate_parameter(3)] = int(self.parameter(1) < self.parameter(2))
+        value = int(self.parameter(1) < self.parameter(2))
+        self.write_to_parameter(3, value)
         self.instruction_pointer += 4
 
     def equals(self):
-        self.intcode[self.immediate_parameter(3)] = int(self.parameter(1) == self.parameter(2))
+        value = int(self.parameter(1) == self.parameter(2))
+        self.write_to_parameter(3, value)
         self.instruction_pointer += 4
 
-    def halt(self):
-        return
-
-    def run_intcode(self, intcode, inputs):
-        instruction_pointer = 0
-        while instruction_pointer < len(intcode):
-            instruction = intcode[instruction_pointer]
-            modes_code, opcode = divmod(instruction, 100)
-            if opcode == 99:
+    def run(self):
+        while self.instruction_pointer < len(self.intcode):
+            if self.opcode == 1:
+                self.add()
+            elif self.opcode == 2:
+                self.multiply()
+            elif self.opcode == 3:
+                self.save_input()
+            elif self.opcode == 4:
+                return self.output()
+            elif self.opcode == 5:
+                self.jump_if_true()
+            elif self.opcode == 6:
+                self.jump_if_false()
+            elif self.opcode == 7:
+                self.less_than()
+            elif self.opcode == 8:
+                self.equals()
+            else:
                 break
-            parameter_types = self.parameter_types_per_opcode.get(opcode, 0)
-            n_parameters = len(parameter_types)
-            parameters = []
-            for i in range(n_parameters):
-                modes_code, mode = divmod(modes_code, 10)
-                parameter = intcode[instruction_pointer + 1 + i]
-                is_write_parameter = parameter_types[i]
-                if mode == 1 or is_write_parameter:
-                    parameters.append(parameter)
-                else:
-                    parameters.append(intcode[parameter])
-            print(instruction, parameters)
-            if opcode == 1:
-                intcode[parameters[2]] = parameters[0] + parameters[1]
-            elif opcode == 2:
-                intcode[parameters[2]] = parameters[0] * parameters[1]
-            elif opcode == 3:
-                if inputs:
-                    intcode[parameters[0]] = inputs.pop(0)
-                else:
-                    print('Input:')
-                    intcode[parameters[0]] = int(input())
-            elif opcode == 4:
-                return parameters[0]
-            elif opcode == 5:
-                if parameters[0]:
-                    instruction_pointer = parameters[1]
-                    continue
-            elif opcode == 6:
-                if not parameters[0]:
-                    instruction_pointer = parameters[1]
-                    continue
-            elif opcode == 7:
-                if parameters[0] < parameters[1]:
-                    intcode[parameters[2]] = 1
-                else:
-                    intcode[parameters[2]] = 0
-            elif opcode == 8:
-                if parameters[0] == parameters[1]:
-                    intcode[parameters[2]] = 1
-                else:
-                    intcode[parameters[2]] = 0
-            instruction_pointer += 1 + n_parameters
+
+
+class Amplifier:
+    def __init__(self, phase, intcode):
+        self.phase = phase
+        self.intcode_program = IntcodeProgram(intcode, [phase])
+
+    def amplify(self, input_signal):
+        self.intcode_program.input_list.append(input_signal)
+        output_signal = self.intcode_program.run()
+        return output_signal
 
 
 def permutate(values):
@@ -159,13 +121,12 @@ def permutate(values):
 
 
 def run_amplifier_feedback_loop(amplifier_program, phase_settings):
-    amplifiers = [Amplifier(amplifier_program[:], phase) for phase in phase_settings]
+    amplifiers = [Amplifier(phase, amplifier_program[:]) for phase in phase_settings]
     signal = 0
     output_signal = 0
     while True:
         for amplifier in amplifiers:
             signal = amplifier.amplify(signal)
-            print(signal)
             if signal is None:
                 return output_signal
         output_signal = signal
