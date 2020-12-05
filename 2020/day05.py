b4d96f4aa5c92dfa46b066c0a730147a7512ad04
Roméo <romeowp@gmail.com>
2020-12-05 11:21:07 +0100

    Readability

--- 2020/day05.py
+++ 2020/day05.py
@@ -1,19 +1,19 @@
-def get_seat_ids_from_file(file_path):
+def get_sorted_seat_ids_from_file(file_path):
     with open(file_path) as file:
-        return sorted([decode_seat_id(line.strip()) for line in file.readlines()])
+        return sorted(decode_seat_id(line.strip()) for line in file.readlines())
 
 
 def decode_seat_id(boarding_pass):
-    return int(''.join([str(int(x == 'B' or x == 'R')) for x in boarding_pass]), 2)
+    return int(''.join('1' if x in ('B', 'R') else '0' for x in boarding_pass), 2)
 
 
 def day05a():
-    seat_ids = get_seat_ids_from_file('input/day05.txt')
+    seat_ids = get_sorted_seat_ids_from_file('input/day05.txt')
     return seat_ids[-1]
 
 
 def day05b():
-    seat_ids = get_seat_ids_from_file('input/day05.txt')
+    seat_ids = get_sorted_seat_ids_from_file('input/day05.txt')
     for seat_index, seat_id in enumerate(seat_ids):
         next_seat = seat_id + 1
         if seat_ids[seat_index + 1] != next_seat:
